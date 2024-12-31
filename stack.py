b9c105757323732877bb404f332b263cd9880612
Balla Kavya <ballakavya594@gmail.com>
2024-12-31 21:36:14 +0530

    Stack

---
+++ stack.py
@@ -0,0 +1,18 @@
+stack=[]
+
+#push operation
+stack.append(1)
+stack.append(2)
+stack.append(3)
+stack.append(4)
+
+print(stack)
+
+#pop operation
+
+stack.pop()
+print(stack)
+
+#peek element
+top=stack[-1]
+print(top)
