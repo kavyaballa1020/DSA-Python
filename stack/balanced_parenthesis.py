c03477724cedd434f6f0c788e7a2070d9e213b2d
Balla Kavya <ballakavya594@gmail.com>
2025-01-03 11:01:43 +0530

    Balanced_parenthesis

---
+++ stack/balanced_parenthesis.py
@@ -0,0 +1,14 @@
+def balanced_parenthesis(str):
+    stack=[]
+    dic_str={')':'(','}':'{',']':'['}
+    for char in str:
+        if char in dic_str:
+           if stack and stack[-1]==dic_str[char]:
+                stack.pop()
+           else:
+               return False
+        else:
+            stack.append(char)
+    return not stack
+print(balanced_parenthesis("({[]})"))
+print("Hello")
