45afe74ed98ef77224498da1e489090c8043b4a1
Balla Kavya <ballakavya594@gmail.com>
2025-01-05 08:45:07 +0530

    Binary Search

--- Searching/Binary_Search.py
+++ Searching/Binary_Search.py
@@ -6,13 +6,14 @@ def binary_search(arr,key):
         if arr[mid] == key:
             return True
         elif arr[mid] < key:
-            right=mid-1
-        else:
             left=mid+1
+        else:
+            right=mid-1
+
     return False
 
 
 arr=[12,23,34,45,56,67,78,89,90]
-key=56
+key=77
 result=binary_search(arr,key)
 print("Result : ",result)
