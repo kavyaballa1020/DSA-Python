3799af8a1246537a73b2ec04f8658224e5527fe4
Balla Kavya <ballakavya594@gmail.com>
2025-04-02 12:00:00 +0530

    Refactor CircularLinkedList implementation; update methods for insertion, deletion, and display logic

--- LinkedList/Circular_linked_list.py
+++ LinkedList/Circular_linked_list.py
@@ -1,83 +1,133 @@
 class node:
-    def __init__(self,data):
-        self.data=data
-        self.next=None
-        
-class circular_linked_list:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+class Circular_linked_list:
     def __init__(self):
-        self.head=None
+        self.tail= None
+        self.size= 0
+    def insertBegin(self,data):
+        new_node= node(data)
+        if self.tail is None:
+            self.tail= new_node
+            self.tail.next= self.tail
+        else:
+            new_node.next= self.tail.next
+            self.tail.next= new_node
+        self.size+= 1
+    def insertEnd(self,data):
+        new_node= node(data)
+        if self.tail is None:
+            self.tail= new_node
+            self.tail.next= self.tail
+        else:
+            new_node.next= self.tail.next
+            self.tail.next= new_node
+            self.tail= new_node
+        self.size+= 1
+        
+    def deleteBegin(self):
+        if self.tail is None:
+            return
+        if self.tail.next== self.tail:
+            self.tail= None
+        else:
+            self.tail.next= self.tail.next.next
+        self.size-= 1
+        
+    def deleteEnd(self):
+        if self.tail is None:
+            return
+        if self.tail.next== self.tail:
+            self.tail= None
+        else:
+            temp= self.tail.next
+            while temp.next!= self.tail:
+                temp= temp.next
+            temp.next= self.tail.next
+            self.tail= temp
+        self.size-= 1
     
-    def insert_at_beginning(self,data):
-        newnode=node(data)
-        if self.head is None:
-            self.head=newnode
-            self.head.next=self.head
-        else:
-            temp=self.head
-            while temp.next != self.head:
-                temp=temp.next
-            newnode.next=self.head
-            self.head=newnode
-            temp.next=self.head
-        
-    def insert_at_end(self,data):
-        newnode=node(data)
-        if self.head is None:
-            self.head=newnode
-            self.head.next=self.head
-        else:
-            temp=self.head
-            while temp.next!= self.head:
-                temp=temp.next
-            temp.next=newnode
-            newnode.next=self.head
-        
-    def delete_at_begin(self):
-        if self.head is None:
-            print("List is empty")
+    def posInsert(self,data,pos):
+        if pos< 0 or pos> self.size:
             return
+        if pos== 0:
+            self.insertBegin(data)
+        elif pos== self.size:
+            self.insertEnd(data)
         else:
-            temp=self.head
-            while temp.next != self.head:
-                temp=temp.next
-            self.head=self.head.next
-            temp.next=self.head
-        
-    def delete_at_end(self):
-        if self.head is None:
-            print("List is empty")
+            new_node= node(data)
+            temp= self.tail.next
+            i=1
+            while i<pos-1:
+                temp= temp.next
+                i+=1
+            new_node.next= temp.next
+            temp.next= new_node
+            self.size+= 1
+            
+    def posDelete(self,pos):
+        if pos< 0 or pos>= self.size:
             return
+        if pos== 0:
+            self.deleteBegin()
+        elif pos== self.size-1:
+            self.deleteEnd()
         else:
-            temp=self.head
-            while temp.next.next !=self.head:
-                temp=temp.next
-            temp.next=self.head
-
+            temp= self.tail.next
+            i=1
+            while i<pos-1:
+                temp= temp.next
+                i+=1
+            temp.next= temp.next.next
+            self.size-= 1
+    
     def display(self):
-        if self.head is None:
-            print("List is empty")
+        if self.tail is None:
             return
-        else:
-            temp=self.head
-            while temp.next != self.head:
-                print(temp.data,end=" --> ")
-                temp=temp.next
-            print(temp.data)
+        temp= self.tail.next
+        while temp!= self.tail:
+            print(temp.data, end=" -> ")
+            temp= temp.next
+        print(self.tail.data, end=" ")
+        print()
+    
+    def reverese(self):
+        if self.tail is None:
+            return
+        prev = None
+        curr = self.tail.next  
+        first = curr          
 
-cll=circular_linked_list()
+        while curr != self.tail:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
 
-cll.insert_at_beginning(10)
-cll.insert_at_beginning(20)
-cll.insert_at_end(30)
-cll.insert_at_end(40)
+        next_node = curr.next
+        curr.next = prev
+        next_node.next = curr  
 
-print("Circular Linked List:")
-cll.display()  
+        self.tail = first     
 
-cll.delete_at_begin()
-print("After deleting from beginning:")
-cll.display()  
-
-cll.delete_at_end()
-print("After deleting from end:")
-cll.display()  
+        
+           
+c=Circular_linked_list()
+c.insertBegin(1)
+c.insertBegin(2)
+c.insertBegin(3)
+c.display()
+c.insertEnd(4)
+c.insertEnd(5)
+c.display()
+c.deleteBegin()
+c.display()
+c.deleteEnd()
+c.display()
+c.posInsert(6,2)
+c.display()
+c.posDelete(2)
+c.display()
+c.reverese()
+c.display()
