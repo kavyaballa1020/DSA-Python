1eb6f8e8434f7c3f224eb5753180d3d8abffaab9
Balla Kavya <ballakavya594@gmail.com>
2025-04-08 16:15:52 +0530

    Implement position-based insertion and deletion methods in DoublyLinkedList; update size accordingly

--- LinkedList/double_linked_list.py
+++ LinkedList/double_linked_list.py
@@ -7,6 +7,7 @@ class Node:
 class DoublyLinkedList:
     def __init__(self):
         self.head = None
+        self.size = 0
         
     def insert_at_beginning(self, data):
         newnode = Node(data)
@@ -16,6 +17,7 @@ class DoublyLinkedList:
         newnode.next = self.head
         self.head.prev = newnode
         self.head = newnode
+        self.size+=1
     
     def insert_at_end(self, data):
         newnode = Node(data)
@@ -27,6 +29,7 @@ class DoublyLinkedList:
             temp = temp.next
         temp.next = newnode
         newnode.prev = temp
+        self.size+=1
         
     def delete_at_begin(self):
         if self.head is None: 
@@ -37,6 +40,7 @@ class DoublyLinkedList:
             return
         self.head = self.head.next
         self.head.prev = None
+        self.size-=1
     
     def delete_at_end(self):
         if self.head is None:  
@@ -49,7 +53,46 @@ class DoublyLinkedList:
         while temp.next: 
             temp = temp.next
         temp.prev.next = None
-        
+        self.size-=1
+    
+    def posInsert(self,data,pos):
+        if pos>self.size+1 or pos<1:
+            print("Invalid")
+            return
+        newnode=Node(data)
+        temp=self.head
+        i=1
+        if pos==1:
+            newnode.next=self.head
+            self.head.prev=newnode
+            self.head=newnode
+            self.size+=1
+            return
+        while i<pos-1:
+            temp=temp.next
+            i+=1
+        newnode.next=temp.next
+        newnode.prev=temp
+        temp.next.prev = newnode
+        temp.next=newnode
+        self.size+=1
+    def posdelete(self,pos):
+        if pos<1 or pos>self.size:
+            print("Invalid")
+            return
+        if pos==1:
+            self.head=self.head.next
+            self.head.prev=None
+            self.size-=1
+            return
+        temp=self.head
+        i=1
+        while i<pos-1:
+            temp=temp.next
+            i+=1
+        temp.next=temp.next.next
+        temp.next.prev = temp
+        self.size-=1
     def display_forward(self):
         if self.head is None:  
             print("List is empty")
@@ -76,19 +119,23 @@ dll = DoublyLinkedList()
 
 dll.insert_at_beginning(10)
 dll.insert_at_beginning(20)
+dll.display_forward()
 dll.insert_at_end(30)
 dll.insert_at_end(40)
+dll.display_forward()
 
 print("Forward Traversal:")
 dll.display_forward()  
-
 print("Backward Traversal:")
 dll.display_backward()  
 
 dll.delete_at_begin()
-print("After deleting from beginning:")
+dll.display_forward() 
+dll.delete_at_end()
 dll.display_forward() 
 
-dll.delete_at_end()
-print("After deleting from end:")
-dll.display_forward()  
+dll.posInsert(25, 3)
+dll.display_forward()
+
+dll.posdelete(1)
+dll.display_forward()
